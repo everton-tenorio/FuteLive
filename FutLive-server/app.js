1a82d26b42bb056f3614b9f7c5b1b983e27cd349
Everton Tenorio <114461163+everton-tenorio@users.noreply.github.com>
2023-09-10 05:55:54 -0300

    Refatoração: Melhorias no código Express e tratamento de erros

--- FutLive-server/app.js
+++ FutLive-server/app.js
@@ -1,51 +1,50 @@
-const express = require('express')
-const axios = require('axios')
-const cheerio = require('cheerio')
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-const app = express()
+const app = express();
+const PORT = process.env.PORT || 3000;
+const URL_GOAL = 'https://www.goal.com/br/listas/futebol-programacao-jogos-tv-aberta-fechada-onde-assistir-online-app/bltc0a7361374657315';
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
-  
-  const url_goal = 'https://www.goal.com/br/listas/futebol-programacao-jogos-tv-aberta-fechada-onde-assistir-online-app/bltc0a7361374657315';
-  
-  app.get('/', (req, res) => {
-    axios.get(url_goal)
-      .then((response) => {
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const jsontexto = $('script[id="__NEXT_DATA__"]').text();
-        const jsonfim = JSON.parse(jsontexto);
-        const x = jsonfim.props.pageProps.content.slideList.slides;
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
-        const tables = x.map((slide, index) => {
-          const tableHtml = slide.body.body;
-          const diaJogo = slide.headline
-          const table = cheerio.load(tableHtml)('table');
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(URL_GOAL);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const json_goal = $('script[id="__NEXT_DATA__"]').text();
+    const json_data = JSON.parse(json_goal);
+    const get_slides = json_data.props.pageProps.content.slideList.slides;
 
-           // Remover linhas que contêm a frase "TEMPO REAL"
-          table.find('th:contains("TEMPO REAL")').remove();
-          table.find('td:contains("Tempo real")').remove();
+    const tables = get_slides.map((data, index) => {
+      const tableHtml = data.body.body;
+      const diaJogo = data.headline;
+      const table = cheerio.load(tableHtml)('table');
 
-          return {
-            title: `Table ${index + 1}`,
-            diaJogo: diaJogo,
-            content: `<table>${table.html()}</table>`,
-          };
-        });
-        res.json(tables);
-      })
-      .catch((error) => {
-        console.error('Erro ao fazer a requisição:', error);
-        res.status(500).json({ error: 'Erro ao buscar as tabelas.' });
-      });
-  });
-  
-  const port = 3000;
-  app.listen(port, () => {
-    console.log(`Servidor rodando em http://0.0.0.0:${port}`);
-  });
+      // Remover linhas que contêm a frase "TEMPO REAL"
+      table.find('th:contains("TEMPO REAL")').remove();
+      table.find('td:contains("Tempo real")').remove();
+
+      return {
+        title: `Table ${index + 1}`,
+        diaJogo,
+        content: `<table>${table.html()}</table>`,
+      };
+    });
+
+    res.json(tables);
+  } catch (error) {
+    console.error('Erro ao fazer a requisição:', error);
+    res.status(500).json({ error: 'Erro ao buscar as tabelas.' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
